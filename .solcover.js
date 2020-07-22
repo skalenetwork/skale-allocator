8113fd3a649862bb515dcf3aedeb6cbeba63e3fa
Artem Payvin <payvinartem@gmail.com>
2020-07-22 11:41:05 +0300

    Remove private keys

--- .solcover.js
+++ .solcover.js
@@ -7,31 +7,7 @@ module.exports = {
     skipFiles: ['thirdparty/', 'interfaces/', 'test/', 'utils/'],
     copyPackages: ['@openzeppelin/contracts'],
     providerOptions: {
-        "accounts": [
-            {
-                "secretKey": process.env.PRIVATE_KEY_1,
-                "balance": "0xd3c21bcecceda1000000"
-            },
-            {
-                "secretKey": process.env.PRIVATE_KEY_2,
-                "balance": "0xd3c21bcecceda1000000"
-            },
-            {
-                "secretKey": process.env.PRIVATE_KEY_3,
-                "balance": "0xd3c21bcecceda1000000"
-            },
-            {
-                "secretKey": process.env.PRIVATE_KEY_4,
-                "balance": "0xd3c21bcecceda1000000"
-            },
-            {
-                "secretKey": process.env.PRIVATE_KEY_5,
-                "balance": "0xd3c21bcecceda0000000"
-            },
-            {
-                "secretKey": process.env.PRIVATE_KEY_6,
-                "balance": "0xd3c21bcecceda0000000"
-            }
-        ]
+        "gas": 100000000,
+        "gasPrice": "0x01"
     }
 };
