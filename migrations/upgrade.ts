6d130215d069cbd8854727d90f7b3eaad806699f
Artem Payvin <payvinartem@gmail.com>
2022-08-19 13:07:18 +0100

    Update Deployed version and upgrade script

--- migrations/upgrade.ts
+++ migrations/upgrade.ts
@@ -73,7 +73,7 @@ async function upgradeCustomEscrows(
 async function main() {
     await upgrade(
         "skale-allocator",
-        "2.2.0",
+        "2.2.2",
         getDeployedVersion,
         setNewVersion,
         contracts,
@@ -83,91 +83,8 @@ async function main() {
             // deploy new contracts
         },
         // async (safeTransactions, abi, contractManager) => {
-        async (safeTransactions) => {
-            let production = false;
-            if (process.env.PRODUCTION === "true") {
-                production = true;
-            }
-        
-            let maxFeePerGas = 100*1e9;
-            let maxPriorityFeePerGas = 1e9;
-            if (hre.network.config.gasPrice !== "auto") {
-                maxFeePerGas = hre.network.config.gasPrice;
-                maxPriorityFeePerGas = hre.network.config.gasPrice;
-            }
-
-            const proxyAdmin = await getManifestAdmin(hre) as ProxyAdmin;
-            const [deployer] = await ethers.getSigners();
-
-            if (production) {
-                if (!process.env.ABI) {
-                    console.log(chalk.red("Set path to file with ABI and addresses to ABI environment variables"));
-                    return;
-                }
-                if (!process.env.NETWORK) {
-                    console.log(chalk.red("Set network type. Example NETWORK=mainnet"));
-                    return;
-                }
-                if (!process.env.ETHERSCAN) {
-                    console.log(chalk.red("Set ETHERSCAN api key"));
-                    return;
-                }
-                await exec(
-                    `ABI=${process.env.ABI} ` +
-                    `NETWORK=${process.env.NETWORK} ` +
-                    `ETHERSCAN=${process.env.ETHERSCAN} ` +
-                    `python3 ${__dirname}/../scripts/get_escrows.py`
-                );
-
-                if (!existsSync(__dirname + "/../data/proxy_list.txt")) {
-                    console.log("PLEASE Provide a proxy_list.txt which contains all escrow proxy addresses.");
-                    process.exit(1);
-                }
-
-                const proxies = (await fs.readFile(__dirname + "/../data/proxy_list.txt", "utf-8"))
-                    .split('\n')
-                    .map((line) => line.trim())
-                    .filter((line) => line !== "")
-
-
-                console.log("Deploy implementation");
-                const escrowFactory = (await ethers.getContractFactory("Escrow")).connect(deployer);
-                const escrow = await escrowFactory.deploy({
-                    maxFeePerGas: maxFeePerGas,
-                    maxPriorityFeePerGas: maxPriorityFeePerGas
-                });
-                console.log("Deploy transaction:");
-                console.log("https://etherscan.io/tx/" + escrow.deployTransaction.hash)
-                console.log("New Escrow address:", escrow.address);
-                await escrow.deployTransaction.wait();
-                await verify("Escrow", escrow.address, []);
-
-                const newImplementationAddress = escrow.address;
-
-                await upgradeCustomEscrows(proxyAdmin, proxies, newImplementationAddress, safeTransactions);
-
-                const implementations = await Promise.all(proxies.map(async (proxy) => {
-                    return await proxyAdmin.getProxyImplementation(proxy);
-                }));
-
-                const distinctImplementations = [...new Set(implementations)];
-                // change to !== 1 after upgrade
-                if (distinctImplementations.length !== 2) {
-                    console.log("Upgraded Escrows have different implementations. Check if Escrow list is correct.");
-                    console.log("Present implementations:");
-                    distinctImplementations.forEach((implementation) => console.log(implementation));
-                    throw Error("Wrong Escrow list");
-                }
-
-                for (const proxy of proxies) {
-                    safeTransactions.push(encodeTransaction(
-                        0,
-                        proxyAdmin.address,
-                        0,
-                        proxyAdmin.interface.encodeFunctionData("upgrade", [proxy, newImplementationAddress])
-                    ));
-                }
-            }
+        async () => {
+            // initialization
         }
     );
 }
