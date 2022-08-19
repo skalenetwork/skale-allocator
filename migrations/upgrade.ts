3ad0e386a703c234cf4752f90b73684afb17c473
Artem Payvin <payvinartem@gmail.com>
2022-08-19 13:12:21 +0100

    Fix upgrade script

--- migrations/upgrade.ts
+++ migrations/upgrade.ts
@@ -1,21 +1,8 @@
-import util from 'util';
 import chalk from "chalk";
 import { contracts } from "./deploy";
-import { promises as fs, existsSync } from "fs";
-import { exec as asyncExec } from "child_process";
-import hre, { ethers } from "hardhat";
-import { getManifestAdmin } from "@openzeppelin/hardhat-upgrades/dist/admin";
-import { ProxyAdmin, Allocator } from "../typechain-types";
-import { upgrade, verify, SkaleABIFile, encodeTransaction, getContractKeyInAbiFile } from "@skalenetwork/upgrade-tools";
-
-const exec = util.promisify(asyncExec);
-
-interface CustomEscrows {
-    [escrow: string]: {
-        oldImplementation: string
-        beneficiary: string
-    }
-}
+import { ethers } from "hardhat";
+import { Allocator } from "../typechain-types";
+import { upgrade, SkaleABIFile, encodeTransaction, getContractKeyInAbiFile } from "@skalenetwork/upgrade-tools";
 
 async function getAllocator(abi: SkaleABIFile) : Promise<Allocator> {
     return ((await ethers.getContractFactory("Allocator")).attach(
@@ -40,34 +27,6 @@ export async function setNewVersion(safeTransactions: string[], abi: SkaleABIFil
         0,
         allocator.interface.encodeFunctionData("setVersion", [newVersion]),
     ));
-}
-
-async function upgradeCustomEscrows(
-    proxyAdmin: ProxyAdmin,
-    proxies: string[],
-    newImplementationAddress: string,
-    safeTransactions: string[]
-) : Promise<void> {
-    const escrowFactory = await ethers.getContractFactory("Escrow");
-    const escrows = JSON.parse(await fs.readFile(__dirname + "/../data/customEscrows.json", "utf-8")) as CustomEscrows;
-    for (const escrow in escrows) {
-        if (escrows[escrow].oldImplementation == await proxyAdmin.getProxyImplementation(escrow)) {
-            if (escrows[escrow].beneficiary == "") {
-                throw Error("Beneficiary wasn't found");
-            }
-            const encodedReinitialize = escrowFactory.interface.encodeFunctionData("reinitialize", [escrows[escrow].beneficiary]);
-            safeTransactions.push(encodeTransaction(
-                0,
-                proxyAdmin.address,
-                0,
-                proxyAdmin.interface.encodeFunctionData("upgradeAndCall", [escrow, newImplementationAddress, encodedReinitialize])
-            ));
-        }
-        const index = proxies.indexOf(escrow);
-        if (~index) {
-            proxies.splice(index, 1);
-        }
-    }
 }
 
 async function main() {
