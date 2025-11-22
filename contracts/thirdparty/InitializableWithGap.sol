00b429bb79aa50e232ded0b134d295972a664816
Vadim Yavorsky <yavrsky@gmail.com>
2025-11-22 14:47:43 +0000

    Upgrade Solidity to 0.8.28

--- contracts/thirdparty/InitializableWithGap.sol
+++ contracts/thirdparty/InitializableWithGap.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
-pragma solidity 0.8.11;
+pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
 
 contract InitializableWithGap is Initializable {
     uint256[50] private ______gap;
