b829297873c94040a00aff41cb1b1d136d7aaae2
Vadim Yavorsky <yavrsky@gmail.com>
2025-11-25 13:52:21 +0000

    Remove ganache-cli from test_deploy script

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -11,7 +11,8 @@ CURRENT_NODE_VERSION=$(nvm current)
 
 git clone --branch stable https://github.com/skalenetwork/skale-manager.git
 echo "Skale manager cloned"
-npx ganache-cli --gasLimit 8000000 --quiet &
+HARDHAT_NODE_SESSION="hardhat-node"
+yarn pm2 start "yarn hardhat node" --name "$HARDHAT_NODE_SESSION"
 
 cd skale-manager
 nvm install $DEPLOYED_WITH_NODE_VERSION
@@ -26,4 +27,4 @@ nvm use $CURRENT_NODE_VERSION
 
 NODE_OPTIONS="--max-old-space-size=4096" npx hardhat run migrations/deploy.ts --network localhost
 
-npx kill-port 8545
+yarn pm2 stop "$HARDHAT_NODE_SESSION"
