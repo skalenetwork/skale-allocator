6b9d15a75b264d7049d6bc262352506a7a2f26fe
Vadim Yavorsky <yavrsky@gmail.com>
2025-11-27 19:14:55 +0000

    Split long lines

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -288,7 +288,11 @@ contract Escrow is IERC777Recipient, IERC777Sender, IEscrow, Permissions {
      *
      * - Beneficiary and Vesting manager must be `msg.sender`.
      */
-    function cancelPendingDelegation(uint delegationId) external override onlyActiveBeneficiaryOrVestingManager {
+    function cancelPendingDelegation(uint256 delegationId)
+        external
+        override
+        onlyActiveBeneficiaryOrVestingManager
+    {
         IDelegationController delegationController = IDelegationController(
             contractManager.getContract("DelegationController")
         );
