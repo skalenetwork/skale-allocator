b43413cd4f458503ecc585476ac528064148c0ea
Vadim Yavorsky <yavrsky@gmail.com>
2026-01-06 19:13:13 +0000

    Remove proxyAdmin from allocator deployment

--- test/tools/deploy/allocator.ts
+++ test/tools/deploy/allocator.ts
@@ -2,12 +2,10 @@ import { ContractManager, Allocator } from "../../../typechain-types";
 import { deployFunctionFactory } from "./factory";
 import { deployTimeHelpersTester } from "./test/timeHelpersTester";
 import { deployEscrow } from "./escrow";
-import { deployProxyAdmin } from "./test/proxyAdmin";
 
 export const deployAllocator
     = deployFunctionFactory("Allocator",
         async (contractManager: ContractManager) => {
             await deployEscrow(contractManager);
             await deployTimeHelpersTester(contractManager);
-            await deployProxyAdmin(contractManager);
         }) as (contractManager: ContractManager) => Promise<Allocator>;
